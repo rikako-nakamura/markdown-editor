53773b466f1c1c92cebc40efda4520b39c327837
rikako <jc180215b@gmail.com>
2024-08-21 16:20:10 +0900

    add_react-router

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import { render } from "react-dom";
 import styled from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import { Editor } from "./pages/editor";
+import { History } from "./pages/history";
+import { 
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
 const GrobalStyle = createGlobalStyle`
 body * {
@@ -13,7 +20,15 @@ body * {
 const Main = (
   <>
     <GrobalStyle />
-    <Editor />
+    <Router>
+      <Route exact path={"/editor"}>
+        <Editor />
+      </Route>
+      <Route exact path={"/history"}>
+        <History />
+      </Route>
+      <Redirect to="/editor" path="*"/>
+    </Router>
   </>
 )
 render(Main, document.getElementById('app'))
